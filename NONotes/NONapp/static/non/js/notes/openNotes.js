cc1f56ec0a6e80b152bdb9c987ea3b99c4cccb1b
rOman4ike <rOman4ike@yandex.ru>
2022-05-10 14:02:43 +0700

    add edit notes, fix open/close notes

--- NONotes/NONapp/static/non/js/notes/openNotes.js
+++ NONotes/NONapp/static/non/js/notes/openNotes.js
@@ -1,11 +1,41 @@
 const openNotes = document.querySelectorAll(".hero__notes-el");
+const closeNotes = document.querySelectorAll('.hero__util-el--close');
+let flag = false;
 
 openNotes.forEach((item) => item.addEventListener('click', () => {
-   item.classList.toggle("hero__notes-el--test");
-   
-   let bar = item.querySelector(".hero__util");
-   (bar.style.display === "block") ? bar.style.display = 'none' : bar.style.display = 'block';
+   if(!flag) {
+      item.classList.add("hero__notes-el--test");
+      let bar = item.querySelector(".hero__util");
+      (bar.style.display === "block") ? bar.style.display = 'none' : bar.style.display = 'block';
 
-   let date = item.querySelector('.hero__notes-date');
-   (date.style.display === "none") ? date.style.display = 'block' : date.style.display = 'none';
+      let date = item.querySelector('.hero__notes-date');
+      (date.style.display === "none") ? date.style.display = 'block' : date.style.display = 'none';
+
+      let textareaDescr = item.querySelector('.hero__notes-descr');
+      (textareaDescr.disabled == true) ? textareaDescr.disabled = false : textareaDescr.disabled = true;
+
+      let textareaHeader = item.querySelector('.hero__notes-header');
+      (textareaHeader.disabled == true) ? textareaHeader.disabled = false : textareaHeader.disabled = true;
+      flag = true;
+   }
+}));
+
+closeNotes.forEach((item) => item.addEventListener('click', () => {
+   if(flag) {
+      openNotes.forEach((item) => item.classList.remove('hero__notes-el--test'))
+      // document.classList.remove("hero__notes-el--test");
+      
+      let bar = item.querySelector(".hero__util");
+      (bar.style.display === "block") ? bar.style.display = 'none' : bar.style.display = 'block';
+   
+      let date = item.querySelector('.hero__notes-date');
+      (date.style.display === "none") ? date.style.display = 'block' : date.style.display = 'none';
+   
+      let textareaDescr = item.querySelector('.hero__notes-descr');
+      (textareaDescr.disabled == true) ? textareaDescr.disabled = false : textareaDescr.disabled = true;
+
+      let textareaHeader = item.querySelector('.hero__notes-header');
+      (textareaHeader.disabled == true) ? textareaHeader.disabled = false : textareaHeader.disabled = true;
+      flag = false;
+   }
 }));
